d021223014efc55c1294e68ad8fa97a829c1e551
nickfrahm <nfrahm24@gmail.com>
2021-10-04 19:01:02 -0500

    continuing UI code

--- src/UI.js
+++ src/UI.js
@@ -1,7 +1,11 @@
+export const initEventListeners = () => {
+  //if needed
+};
+
 export const createBoard = (name, length, parent) => {
   let boardContainer = document.createElement('div');
   boardContainer.classList.add('board-container');
-  boardContainer.id = name;
+  boardContainer.id = `${name}-board`;
 
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < length; j++) {
@@ -15,4 +19,17 @@ export const createBoard = (name, length, parent) => {
   }
 
   parent.appendChild(boardContainer);
+};
+
+export const showShipPlacement = (len) => {
+  let shipUI = document.createElement('div');
+  shipUI.className('ship-tile-container');
+
+  for (let i = 0; i < len; i++) {
+    let shipTile = document.createElement('p');
+    shipTile.classList.add('tile', 'ship-tile');
+    shipUI.appendChild(shipTile);
+  }
+
+  return shipUI;
 };
